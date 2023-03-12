b27ec4698ddff3e99b0a11edb6c8ea71bc0f6789
Ben Thomson <git@alfreido.com>
2023-03-12 19:32:41 +0800

    WIP User plugin testing

--- winter.mix.js
+++ winter.mix.js
@@ -4,7 +4,8 @@ mix.setPublicPath(__dirname);
 
 // Render Tailwind style
 mix.postCss('assets/css/base.css', 'assets/css/theme.css', [
-  require('postcss-import'),
-  require('tailwindcss'),
-  require('tailwindcss/nesting'),
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
+    require('autoprefixer')
 ]);
